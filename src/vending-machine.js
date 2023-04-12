a2f71a20985f1a4a05731fd2cb5e3dd8855f7280
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-04-12 18:54:03 +0530

    added features of denominations in any order

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -6,14 +6,28 @@ const countCoins = function(amount, denomination) {
   return Math.floor(amount / denomination);
 }
 
+const sort = function(list) {
+  for (let i = 0; i < list.length - 1; i++){
+    for (let j = i + 1; j < list.length; j++) {
+      if (list[i] > list[j]){
+        const temp = list[i];
+        list[i] = list[j];
+        list[j] = temp;
+      }
+    }
+  }
+  return list
+}
+
 const countOptimumCoins = function(amount, denominations) {
   let leftOverAmount = amount;
   let coins = 0;
+  const sortedDenominations = sort(denominations);
 
-  for (let index = denominations.length - 1; index >= 0; index--) {
+  for (let index = sortedDenominations.length - 1; index >= 0; index--) {
     if (leftOverAmount === 0) break;
 
-    const currentDenomination = denominations[index];
+    const currentDenomination = sortedDenominations[index];
     coins += countCoins(leftOverAmount, currentDenomination);
     leftOverAmount = calculateRemainingAmount(leftOverAmount, currentDenomination);
   }
--- tests/vending-machine-test.js
+++ tests/vending-machine-test.js
@@ -19,10 +19,12 @@ const testdispenseCoins = function() {
   assertEquals(3, dispenseCoins(8, [1, 2, 5, 10]), "8 rupees should return 3 coins");
   assertEquals(3, dispenseCoins(9, [1, 2, 5, 10]), "9 rupees should return 3 coins");
   assertEquals(4, dispenseCoins(18, [1, 2, 5, 10]), "18 rupees should return 4 coins");
-  assertEquals(5, dispenseCoins(18, [1, 2, 5]), "18 rupees should return 5 coins of denominations [1, 2, 5]");
+  assertEquals(5, dispenseCoins(18, [1, 2, 5]), "18 rupees should return 5 coins");
   assertEquals(9, dispenseCoins(18, [1, 2]), "18 rupees should return 9 coins of two");
   assertEquals(3, dispenseCoins(18, [5]), "18 rupees should return 3 coins of five");
-  assertEquals(4, dispenseCoins(13, [1, 4, 7]), "13 rupees should return 4 coins");
+  assertEquals(4, dispenseCoins(13, [7, 4, 1]), "13 rupees should return 4 coins");
+  assertEquals(5, dispenseCoins(18, [5, 2, 1]), "18 rupees should return 5 coins");
+  assertEquals(5, dispenseCoins(18, [5, 1, 2]), "18 rupees should return 5 coins");
   summary();
 }
 
